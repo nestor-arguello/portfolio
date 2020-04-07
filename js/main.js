f06fd9dcf441f78e1a6ab020c56324aad399edb7
Nestor Arguello <nestor.arguello@gmail.com>
2020-04-07 17:09:27 -0400

    add homepage

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function() {
+
+  // const cards = document.querySelectorAll('.card');
+
+  // cards.forEach(content => {
+  //   content.addEventListener('transitionend', function(event) {
+  //     const cardContent = this.querySelector('.card__content');
+
+  //     cardContent.classList.add('show');
+  //   })
+  // })
+
+})();
