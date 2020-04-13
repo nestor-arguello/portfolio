52c938f69ec2c70cd61def48a8640b8bd1b32aca
Nestor Arguello <nestor.arguello@gmail.com>
2020-04-13 18:42:22 -0400

    change landing page & add react-ecommerce, react-skypey and react-vehicleslist projects

---
+++ works/react-ecommerce/precache-manifest.fc22fceadf12cf5a4ecd35bfee58ae21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9339bbf60815159c9878f44f326cb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f499723378dc6cdec991",
+    "url": "/static/css/main.ac816686.chunk.css"
+  },
+  {
+    "revision": "2c4e8d08cda0827f1f2e",
+    "url": "/static/js/2.7accfb28.chunk.js"
+  },
+  {
+    "revision": "4baf5dc16be53e517e8b1978976d5788",
+    "url": "/static/js/2.7accfb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f499723378dc6cdec991",
+    "url": "/static/js/main.77071c19.chunk.js"
+  },
+  {
+    "revision": "e833fd9bf6dddec7a6d4",
+    "url": "/static/js/runtime-main.4ef518df.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/11.2 shopping-bag.svg.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/4.3 crown.svg.ed28accb.svg"
+  }
+]);
